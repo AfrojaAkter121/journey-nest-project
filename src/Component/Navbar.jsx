e11ada45018d17284d825f3e4c4a9719e8cc4813
afroja akter <afrojaakter10bd@gmail.com>
2025-06-05 11:38:54 +0600

    added profile menu

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,8 +2,8 @@ import { Bell, Menu, Sun } from "lucide-react";
 import { use, useState } from "react";
 import { Link, NavLink } from "react-router";
 import { AuthContext } from "../Context/AuthProvider";
-import { motion } from "motion/react"
-import SlideHoverButton from '../Pages/SlideHoverButton';
+import { motion } from "motion/react";
+import SlideHoverButton from "../Pages/SlideHoverButton";
 
 export default function Navbar() {
   const { user, logOut } = use(AuthContext);
@@ -13,38 +13,38 @@ export default function Navbar() {
     setMenu(!menu);
   };
 
-  const handleLogout =() => {
-    logOut().then(() => {
-      console.log('logout successful')
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+  const handleLogout = () => {
+    logOut()
+      .then(() => {
+        console.log("logout successful");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const links = (
     <div className="flex flex-col md:flex-row items-center italic gap-5  py-2">
-      <SlideHoverButton text={'Home'} link={''}></SlideHoverButton>
-      <SlideHoverButton text={'Add Blog'} link={'addBlogs'}></SlideHoverButton>
-      <SlideHoverButton text={'AllBlogs'} link={'allBlogs'}></SlideHoverButton>
-      <SlideHoverButton text={'Featured'} link={'featured'}></SlideHoverButton>
-      <SlideHoverButton text={' Wishlist'} link={'wishList'}></SlideHoverButton>
-     
+      <SlideHoverButton text={"Home"} link={""}></SlideHoverButton>
+      <SlideHoverButton text={"Add Blog"} link={"addBlogs"}></SlideHoverButton>
+      <SlideHoverButton text={"AllBlogs"} link={"allBlogs"}></SlideHoverButton>
+      <SlideHoverButton text={"Featured"} link={"featured"}></SlideHoverButton>
+      <SlideHoverButton text={" Wishlist"} link={"wishList"}></SlideHoverButton>
     </div>
   );
   return (
-    <motion.nav 
-    initial={{y : -100}}
-    animate={{y: 0}}
-    transition={{duration:2}}>
+    <motion.nav
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 2 }}
+    >
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-2 rounded-lg ">
         {/* Left: Logo */}
         <motion.div
-          initial={{scale: [1, 0.9, 1]}}
-          animate={{scale: [0.9, 1, 0.9]}}
-          transition={{duration:2, delay:3, repeat: Infinity}}
+          initial={{ scale: [1, 0.9, 1] }}
+          animate={{ scale: [0.9, 1, 0.9] }}
+          transition={{ duration: 2, delay: 3, repeat: Infinity }}
           className="flex py-2 pr-10 pl-4 items-center gap-3 bg-[#525e04] rounded-2xl"
-          
         >
           <img
             src="https://i.ibb.co/MknWVzH4/guard-travel-logo-icon-design-vector-22948229.jpg"
@@ -69,20 +69,36 @@ export default function Navbar() {
           <div className="flex z-20 items-center gap-2 border border-[#1D3D2F] rounded-xl px-2 py-1">
             <Menu onClick={handleMenuBar} className="w-6 h-6 text-[#1D3D2F]" />
             {menu && (
-              <div className="w-56 absolute z-10 bg-[#D7E95D]/60 rounded-xl p-5 flex flex-col gap-3 top-18 md:right-26 right-0 justify-center  ">
-                <div className="md:hidden flex ">{links}</div>
+              <div className="w-48 absolute z-10 bg-[#D7E95D]/60 rounded-xl p-5 flex flex-col gap-3 top-18 md:right-8 right-0 justify-center  ">
+
+              {
+                user && <div className="bg-white/50 flex flex-col justify-center items-center p-5 rounded-lg">
+                  <img src={user.photoURL} className="w-20 h-20 rounded-full object-cover border-2 border-green-800" alt="" srcset=""/>
+                  <h1>{user.displayName}</h1>
+                  <h1>{user.email}</h1>
+                </div>
+              }
+
+                <div className="md:hidden flex border-t border-dashed pt-3 ">
+                  {links}
+                  </div>
 
                 {user ? (
-                  <button
-                    onClick={() => {
-                      handleLogout()
-                    }}
-                    className="bg-green-900 text-white py-2 px-2 rounded-2xl w-[80%] italic"
-                  >
-                    Log Out
-                  </button>
+                  <div className="flex flex-col gap-4 justify-center text-center italic">
+                    <div className="border-dashed border"></div>
+                    <SlideHoverButton text={"My Profile"} link={"myProfile"}></SlideHoverButton>
+                    <button
+                      onClick={() => {
+                        handleLogout();
+                      }}
+                      className="bg-green-900 text-white py-2 px-2 rounded-2xl  italic"
+                    >
+                      Log Out
+                    </button>
+                  </div>
                 ) : (
                   <div className="flex flex-col gap-3">
+                    <div className="border-dashed border"></div>
                     <Link onClick={() => console.log("clicked")} to="/signin">
                       <button className=" bg-green-900 text-white py-2 px-2 rounded-2xl w-[80%] italic">
                         Sign In
@@ -99,16 +115,16 @@ export default function Navbar() {
               </div>
             )}
 
-            <div className="avatar avatar-online">
-              <div className="w-10 rounded-full">
+            <div className="">
+              <div className="">
                 {user ? (
-                  <div className="relative group">
+                  <div className="relative group ">
                     <img
                       src={user.photoURL}
                       alt="Profile"
-                      className="cursor-pointer rounded-full"
+                      className="cursor-pointer w-10 h-10 object-cover rounded-full"
                     />
-                    <div className="absolute top-120 left-0 bg-white text-black p-2 rounded shadow hidden group-hover:block z-50">
+                    <div className="absolute top-10 left-0 text-green-800 bg-[#D7E95D] p-2 rounded shadow hidden group-hover:block z-50">
                       {user.displayName}
                     </div>
                   </div>
